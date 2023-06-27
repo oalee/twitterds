1a222160a89fa7b7847ba4db1aa4f745b1206ea7
oalee <oalee@proton.me>
2023-06-27 13:10:08 +0200

    updates

--- twitter/data/pipeline/clean_graph.py
+++ twitter/data/pipeline/clean_graph.py
@@ -9,14 +9,22 @@ env = yerbamate.Environment()
 
 period = env.action if env.action != None else "before"
 
+
+
 edges_path = os.path.join(env["plots"], "analysis", f"edges_{period}.parquet")
+
+print("Loading DataFrame...",  edges_path)
 
 
 df = pd.read_parquet(edges_path)
 
+
+print("Cleaning DataFrame...")
+
 # Sort DataFrame
 df = df.sort_values(['source', 'target'])
 
+print("Grouping DataFrame...")
 # Group by 'source' and 'target', and then count unique 'hashtags'
 df_grouped = df.groupby(['source', 'target']).agg({'hashtag': 'nunique'})
 
@@ -25,6 +33,7 @@ df_grouped = df.groupby(['source', 'target']).agg({'hashtag': 'nunique'})
 df_grouped = df_grouped.reset_index()
 
 
+print("Filtering DataFrame...")
 # drop hashtag count 1 and duplicates
 
 df_grouped = df_grouped[df_grouped['hashtag'] > 1]
@@ -35,4 +44,9 @@ df_grouped = df_grouped.drop_duplicates(['source', 'target'])
 save_path = os.path.join(env['plots'], 'analysis', f'edges_{period}_cleaned.parquet')
 
 
+print("Saving DataFrame...", save_path)
+
 df_grouped.to_parquet(save_path)
+
+
+print("Saved! :)")
