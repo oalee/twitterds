fcff06ad7552a742f635ce26e2ed5c248d638198
oalee <oalee@proton.me>
2023-06-30 20:38:25 +0200

    updates

--- twitter/data/pipeline/user_hashtag.py
+++ twitter/data/pipeline/user_hashtag.py
@@ -1,8 +1,25 @@
 from ..loader.spark import get_tweets_session
-import os, yerbamate
+import os
 from pyspark.sql.functions import col, explode
 
-env = yerbamate.Environment()
+from yerbamate import env
+
+# env = yerbamate.Environment()
+
+
+period = env.get_hparam("period", "after")
+#import ipdb; ipdb.set_trace()
+print(period)
+
+if period == "after":
+    time_filter = ((col("date") > "2022-09-14") & (col("date") < "2023-04-17"))
+
+elif period == "before":
+    time_filter = ((col("date") > "2022-01-01") & (col("date") < "2022-09-14"))
+elif period == "all":
+    time_filter = ((col("date") > "2022-01-01" )& (col("date") < "2023-04-17"))
+else:
+    raise ValueError("Invalid period")
 
 columns = ["userId", "hashtags", "date"]
 
@@ -13,7 +30,9 @@ parquet_df = get_tweets_session(columns)
 parquet_df.na.drop(subset=["hashtags"])
 
 # date after 2022
-parquet_df = parquet_df.filter((col("date") > "2022-01-01") & (col("date") < "2022-09-14"))
+parquet_df = parquet_df.filter(
+    time_filter
+)
 parquet_df = parquet_df.select("userId", explode(col("hashtags")).alias("hashtag"))
 
 # drop hashtags column
@@ -23,5 +42,5 @@ parquet_df = parquet_df.groupBy("userId", "hashtag").count()
 
 # save parquet, mode overwrite if exists
 parquet_df.write.mode("overwrite").parquet(
-    os.path.join(env["plots"], "analysis", "user_hashtag_before.parquet")
+    os.path.join(env["plots"], "analysis", f"user_hashtag_{period}.parquet")
 )
